a6aa33773c021c7b855bdd0b33623dd7f0bda3e2
Jason Phillips <jason.phillips@axuall.com>
2023-12-02 10:23:06 -0600

    day2 part2

--- 2023/day2.py
+++ 2023/day2.py
@@ -24,6 +24,32 @@ def count_cubes(cube_parts):
         
     return current_cube_count
 
+def count_minimun_cubes(cube_parts, current_cube_count):
+    parts = strip_space(cube_parts.split(","))
+    for part in parts:
+        if "blue" in part:
+            if(current_cube_count["blue"] < int(part.split(" ")[0])):
+                current_cube_count["blue"] = int(part.split(" ")[0])
+        if "red" in part:
+            if(current_cube_count["red"] < int(part.split(" ")[0])):
+                current_cube_count["red"] = int(part.split(" ")[0])
+        if "green" in part:
+            if(current_cube_count["green"] < int(part.split(" ")[0])):
+                current_cube_count["green"] = int(part.split(" ")[0])
+        
+    return current_cube_count
+
+def get_cube_minimum(line):
+    cube_mins = {"red": 0, "green": 0, "blue": 0}
+    parts = cube_parts(line)
+    for part in parts:
+        cube_mins = count_minimun_cubes(part, cube_mins)
+    
+    return cube_mins
+
+def get_cube_power(cube_mins):
+    return cube_mins["red"] * cube_mins["green"] * cube_mins["blue"]
+
 def is_valid_game(cubes):
     return  cubes["red"] <= valid_game["red"] and \
             cubes["blue"] <= valid_game["blue"] and \
@@ -44,12 +70,13 @@ def get_game_number(line):
     return line.split(":")[0].split(" ")[1]
 
 def exec(data):
-    valid_count = 0
+    count = 0
     for line in data.split("\n"):
-        if is_line_valid(line):
-            valid_count += int(get_game_number(line))
-            print(line, "is valid")
-    return valid_count
+        count += get_cube_power(get_cube_minimum(line))
+        # if is_line_valid(line):
+        #     valid_count += int(get_game_number(line))
+        #     print(line, "is valid")
+    return count
 
 if __name__ == "__main__":
     with open(os.path.join(here, "../data/2023/2.txt"), "r") as data:
--- 2023/tests/test_day2.py
+++ 2023/tests/test_day2.py
@@ -1,6 +1,6 @@
 import unittest
 
-from day2 import count_cubes, cube_parts, get_game_number, is_line_valid, is_valid_game, exec
+from day2 import count_cubes, cube_parts, get_game_number, is_line_valid, is_valid_game, exec, count_minimun_cubes, get_cube_minimum, get_cube_power
 
 
 class TestDay2(unittest.TestCase):
@@ -10,11 +10,6 @@ class TestDay2(unittest.TestCase):
         self.assertEqual(ans, ["3 blue, 4 red", "1 red, 2 green, 6 blue", "2 green"])
 
     def test_count_cubes(self):
-
-        # self.assertEqual(count_cubes("3 blue, 4 red", {"red": 0, "green": 0, "blue": 0}), {"red": 4, "blue": 3, "green": 0})
-        # self.assertEqual(count_cubes("1 red, 2 green, 6 blue", {"red": 0, "green": 0, "blue": 0}), {"red": 1, "blue": 6, "green": 2})
-        # self.assertEqual(count_cubes("1 red, 2 red, 6 blue", {"red": 0, "green": 0, "blue": 0}), {"red": 3, "blue": 6, "green": 0})
-
         self.assertEqual(count_cubes("3 blue, 4 red"), {"red": 4, "blue": 3, "green": 0})
         self.assertEqual(count_cubes("1 red, 2 green, 6 blue"), {"red": 1, "blue": 6, "green": 2})
         self.assertEqual(count_cubes("1 red, 2 red, 6 blue"), {"red": 3, "blue": 6, "green": 0})
@@ -35,6 +30,24 @@ class TestDay2(unittest.TestCase):
         self.assertEqual(get_game_number("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"), "1")
         self.assertEqual(get_game_number("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"), "2")
         self.assertEqual(get_game_number("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"), "5")
+
+    def test_count_minimun_cubes(self):
+        self.assertEqual(count_minimun_cubes("3 blue, 4 blue", {"red": 0, "green": 0, "blue": 0}), {"red": 0, "blue": 4, "green": 0})
+    
+    def test_count_minimum_per_line(self):
+        self.assertEqual(get_cube_minimum("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"), {"red": 4, "blue": 6, "green": 2})
+        self.assertEqual(get_cube_minimum("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"), {"red": 1, "blue": 4, "green": 3})
+        self.assertEqual(get_cube_minimum("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"), {"red": 20, "blue": 6, "green": 13})
+        self.assertEqual(get_cube_minimum("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"), {"red": 14, "blue": 15, "green": 3})
+        self.assertEqual(get_cube_minimum("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"), {"red": 6, "blue": 2, "green": 3})
+
+    def test_powers(self):
+        self.assertEqual(get_cube_power(get_cube_minimum("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")), 48)
+        self.assertEqual(get_cube_power(get_cube_minimum("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue")), 12)
+        self.assertEqual(get_cube_power(get_cube_minimum("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red")), 1560)
+        self.assertEqual(get_cube_power(get_cube_minimum("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red")), 630)
+        self.assertEqual(get_cube_power(get_cube_minimum("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green")), 36)
+
         
     def test_part1(self):
         test_data = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
@@ -43,4 +56,5 @@ Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
 
-        self.assertEqual(exec(test_data), 8)
+        # self.assertEqual(exec(test_data), 8)
+        self.assertEqual(exec(test_data), 2286)
