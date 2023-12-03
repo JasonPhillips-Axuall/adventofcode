ddad103b465c280564e36540acb68caf08458e02
Jason Phillips <jason.phillips@axuall.com>
2023-12-03 16:41:36 -0600

    test that would have helped

--- 2023/tests/test_day3.py
+++ 2023/tests/test_day3.py
@@ -29,29 +29,18 @@ class TestDay2(unittest.TestCase):
 
 
     def test_has_adjacent_symbol(self):
-        # self.assertTrue(has_symbol_between("...*......", 1, 3, symbol_regex))
+        self.assertTrue(has_symbol_between("...*......", 1, 3, symbol_regex))
         
-        # self.assertFalse(has_symbol_between("617*......", 7,9, symbol_regex))
-        # self.assertFalse(has_symbol_between(".....+.58.", 7,9, symbol_regex))
-        # self.assertFalse(has_symbol_between("..592.....", 7,9, symbol_regex))
+        self.assertFalse(has_symbol_between("617*......", 7,9, symbol_regex))
+        self.assertFalse(has_symbol_between(".....+.58.", 7,9, symbol_regex))
+        self.assertFalse(has_symbol_between("..592.....", 7,9, symbol_regex))
 
         # self.assertEqual(exec("467..114..\n...*......"), 467)
         # self.assertEqual(exec("...*......\n..35..633."), 35)
         # self.assertEqual(exec("......755.\n...$.*...."), 755)
         # self.assertEqual(exec("467..114..\n...*......\n..35..633."), 16345)
         
-        # self.assertEqual(exec("......755.\n...$.*....\n.664.598.."), 451490)
+        self.assertEqual(exec("......755.\n...$.*....\n.664.598.."), 451490)
+        self.assertEqual(exec("2*2"), 4)
+
         self.assertEqual(exec(test_data), 467835)
-        self.assertEqual(exec(""".........677..
-.%..863..#....
-46...*.....475
-......470.*...
-..........587."""), (863*470)+(475*587))
-        
-        self.assertEqual(exec("""..730..35..
-......*....
-......453.."""), 35*453)
-        
-        self.assertEqual(exec("""863..#
-.*....
-..470."""), 863*470)
