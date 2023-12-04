d3d38a3a0bd105f6939693a8c329df3d06491ad3
Jason Phillips <jason.phillips@axuall.com>
2023-12-04 16:01:59 -0600

    day4 part 2

--- 2023/day4.py
+++ 2023/day4.py
@@ -2,6 +2,9 @@
 
 import re
 
+def get_card_number(line):
+    return int(line.split(":")[0].split()[1])
+
 def get_winning(line):
     return set([int(s.strip()) for s in re.findall(r'\d+', line.split(' | ')[1])])
 
@@ -17,16 +20,54 @@ def calc_winning_points(winning_number):
 
     return 2 ** (winning_number -1)
 
+def add_card(cache):
+    retval = []
+    queue = [k for k in cache.keys()]
+    while len(queue) > 0:
+        card_number = queue.pop(0)
+        retval.append(card_number)
+        if card_number in cache:
+            multiplier = retval.count(card_number)
+            
+            if(multiplier == 0):
+                multiplier = 1
+
+            add_to_cache = []
+            for i in range(0, multiplier):
+                add_to_cache += cache[card_number]
+
+            cache[card_number] = add_to_cache
+            tmp = retval + add_to_cache
+            retval = tmp
+    
+    return len(retval)
+
+
 def main(data):
+
+    cache = {}
+
     score = 0
+
+    # calc points
     for line in data.split("\n"):
         game = line.split(": ")[1]
         winning_numbers = get_winning(game)
         player_numbers = get_player(game)
-        win_count = wining_count(set(winning_numbers), set(player_numbers))
-        score += calc_winning_points(win_count)
+        matches = wining_count(set(winning_numbers), set(player_numbers))
+        
+        card_number = get_card_number(line)
+        cache[card_number] = [i+card_number+1 for i in range(0, matches)]
+        
+        points = calc_winning_points(matches)
+            
+        score += points
 
-    return score
+    card_count = add_card(cache)
+
+    # calc cards
+
+    return score, card_count
 
 if __name__ == "__main__":
     import os
--- 2023/tests/test_day4.py
+++ 2023/tests/test_day4.py
@@ -28,11 +28,11 @@ class TestDay4(unittest.TestCase):
         self.assertEqual(calc_winning_points(4), 8)
     
     def test_main(self):
-        self.assertEqual(main("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"), 8)
-        self.assertEqual(main("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19"), 2)
-        self.assertEqual(main("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1"), 2)
-        self.assertEqual(main("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83"), 1)
-        self.assertEqual(main("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36"), 0)
-        self.assertEqual(main("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"), 0)
+        self.assertEqual(main("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53")[0], 8)
+        self.assertEqual(main("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19")[0], 2)
+        self.assertEqual(main("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1")[0], 2)
+        self.assertEqual(main("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83")[0], 1)
+        self.assertEqual(main("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36")[0], 0)
+        self.assertEqual(main("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11")[0], 0)
 
-        self.assertEqual(main(test_data), 13)
+        self.assertEqual(main(test_data)[0], 13)
