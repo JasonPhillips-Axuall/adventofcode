26f7e0d3eb5245d6e037e3fe8b23fc868cef2f2b
Jason Phillips <jason.phillips@axuall.com>
2023-12-09 08:22:04 -0600

    day 8 part 2

--- 2023/day8.py
+++ 2023/day8.py
@@ -1,68 +1,24 @@
+import math
 import re
-
-class node:
-    def __init__(self, data):
-        self.right = None
-        self.left = None
-        self.data = data
-
-    def __eq__(self, other):
-        if not isinstance(other, node):
-            return False
-
-        return self.data == other.data
 
 def build(data):
     retval = {}
     lines = data.split('\n')
     for line in lines:
-        line_parts = re.findall(r'[a-zA-Z]+', line)
+        line_parts = re.findall(r'[a-zA-Z0-9]+', line)
         retval[line_parts[0]] = line_parts[1:]
 
     return retval
-
-def build_tree(tree_string):
-    root = node('AAA')
-    queue = [root]
-    done = []
-    while len(queue) > 0:
-        current_node = queue.pop(0)
-        left = node(tree_string[current_node.data][0])
-        right = node(tree_string[current_node.data][1])
-        if current_node == left == right:
-            break
-
-        current_node.left = left
-        current_node.right = right
-
-        if left in queue:
-            continue
-        
-        queue.append(current_node.left)
-
-        if right in queue: 
-            continue
-
-        queue.append(current_node.right)
-
-    return root
 
 def has_more_children(hash, key):
     if key == hash[key][0] == hash[key][1]:
         return False
     return True
-    
-
-def main(data):
-    instructions = data.split('\n\n')[0]
-    tree_string = build(data.split('\n\n')[1])
-    # tree = build_tree(tree_string)
-    # current_node = tree
+  
+def run(instructions, tree_string, current_node):
     step = 0
-    current_node = 'AAA'
-    while current_node != 'ZZZ':
+    while current_node[-1] != 'Z':
         for i in instructions:
-            
             if i == 'R':
                 current_node = tree_string[current_node][1]
             elif i == 'L':
@@ -71,7 +27,17 @@ def main(data):
             step += 1
     return step
 
+def main(data):
+    instructions = data.split('\n\n')[0]
+    tree_string = build(data.split('\n\n')[1])
+    # tree = build_tree(tree_string)
+    # current_node = tree
+    runs = []
+    keys = [k for k in tree_string.keys() if k[-1] == 'A']
+    for k in keys:
+        runs.append(run(instructions, tree_string, k))
 
+    return math.lcm(*runs)
 
 if __name__ == '__main__':
     import os
--- 2023/tests/test_day8.py
+++ 2023/tests/test_day8.py
@@ -18,8 +18,20 @@ AAA = (BBB, BBB)
 BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)"""
 
+test_data3 = """LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)"""
+
 class TestDay8(unittest.TestCase):
     
     def test_main(self):
         # self.assertEqual(main(test_data), 2)
-        self.assertEqual(main(test_data2), 6)
+        # self.assertEqual(main(test_data2), 6)
+        self.assertEqual(main(test_data3), 6)
